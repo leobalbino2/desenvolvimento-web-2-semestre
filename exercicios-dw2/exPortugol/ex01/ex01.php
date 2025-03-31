6eb7f502991715efd3d1c03b53c390d03e977e09
Leonardo Balbino <leonardo.balbino2@fatec.sp.gov.br>
2025-03-31 18:20:23 -0300

    Commit da pasta de exercícios

---
+++ exercicios-dw2/exPortugol/ex01/ex01.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 01 - Olá Mundo em PHP!</title>
+</head>
+
+<body>
+    <h1 id="texto"> Aperte o botão e insira o texto! </h1>
+    <button onclick="inserirTexto()">Aperte para inserir o texto</button>
+    
+    <script src="./js/ex01.js"></script>
+</body>
+</html>
