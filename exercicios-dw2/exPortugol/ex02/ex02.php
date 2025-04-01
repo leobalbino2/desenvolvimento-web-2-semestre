1b3c700ffa5b32a1bbeb9209547aed7cf302ade7
Leonardo Balbino <leonardo.balbino2@fatec.sp.gov.br>
2025-04-01 09:03:04 -0300

    Adição arquivo CSS em todos os exercícios

--- exercicios-dw2/exPortugol/ex02/ex02.php
+++ exercicios-dw2/exPortugol/ex02/ex02.php
@@ -4,15 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 01 - Olá Mundo em PHP!</title>
+    <link rel="stylesheet" type="text/css" href="../../css/style.css" />
 </head>
 
 <body>
     <?php 
 
-    $mensagem = "Aperte o botão para inserir o texto";
+    $conta = 0;
 
     if(isset($_POST['btn-sbmt'])) {
-        $mensagem = 'Olá Mundo!';
+        ;
     }
     ?>
 
